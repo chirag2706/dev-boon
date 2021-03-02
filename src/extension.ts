d32ad0bb32e480509b278bd0fe7f3b6faac26ded
chirag2706 <cs18b006@iittp.ac.in>
2021-03-03 01:09:50 +0530

    parsing of selected code from editor has been done as well as activation and deactivation of extension functionality has also been done.

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,124 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let isExtensionActivated = 0; // o means thaT initially ,it is deactivated
+var catSmiley = String.fromCodePoint(0X0001F638);
+
+
+
+async function check(context: vscode.ExtensionContext):Promise<string | undefined>{
+	while(context.subscriptions.length>0){
+		context.subscriptions.pop();
+	}
+	let answer = await vscode.window.showInformationMessage(`Extension dev-boon has not been activated 😣\nDo you want to activate it?`,"YES","NO");
+
+	if(answer !== undefined && await answer === "YES"){
+		isExtensionActivated = 1;
+		await vscode.window.showInformationMessage(`Your extension has been activated successfully ${catSmiley} with ${isExtensionActivated} `);
+		
+		activate(context);
+	}else if(answer === "NO"){
+		await vscode.window.showErrorMessage("Sorry to hear that 😣");
+	}else{
+		await vscode.window.showWarningMessage("Something went wrong 😣");
+	}
+	return "";
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dev-boon" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dev-boon.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		let deactivateCommand = vscode.commands.registerCommand("dev-boon.DEACTIVATE_EXTENSION",()=>{
+			
+			if(isExtensionActivated === 1){
+				deactivate(context);
+			}else{
+				check(context);
+			}
+			
+		});
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dev-boon!');
-	});
+		context.subscriptions.push(deactivateCommand);
 
-	context.subscriptions.push(disposable);
+		let stackOverFlowSearchBySelectingTextFromEditor = vscode.commands.registerCommand('dev-boon.STACKOVERFLOW_SEARCH_WITH_SELECTED_TEXT', async () => {
+			if(isExtensionActivated === 1){
+				try {
+					let selectedText = await getSelectedTextFromEditor();
+	
+					vscode.window.showInformationMessage(`Working fine with ${isExtensionActivated}`);
+	
+				} catch (err) {
+					await vscode.window.showErrorMessage("Some Error occured while searching stackOverFlow posts 😣.Please try again");
+				}
+			}else{
+				check(context);
+			}
+
+		});
+
+		context.subscriptions.push(stackOverFlowSearchBySelectingTextFromEditor);
+
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(context: vscode.ExtensionContext) {
+	isExtensionActivated = 0;
+	while(context.subscriptions.length>0){
+		context.subscriptions.pop();
+	}
+	vscode.window.showInformationMessage(`dev-boon extension has been deactivated successfully ${catSmiley} with ${isExtensionActivated}`);
+}
+
+
+
+
+//function which parses editor and gives only that text which is currently selected
+function getSelectedTextFromEditor(): string|undefined{
+	let activeEditor = vscode.window.activeTextEditor;
+
+	if(activeEditor === null || activeEditor === undefined){
+		return "";
+	}
+
+	let editorDocument = activeEditor.document;
+
+	let finalSelectedString = "";
+
+	let eol = "\n";
+
+	if(editorDocument.eol !==1){
+		eol = "\r\n";
+	}
+
+	let fetchSelectedLines: string[] = [];
+
+	activeEditor.selections.forEach((selectedTextLine)=>{
+		if(selectedTextLine.start.line === selectedTextLine.end.line && selectedTextLine.start.character === selectedTextLine.end.character){
+			let range = editorDocument.lineAt(selectedTextLine.start).range;
+			let text = "undefined";
+			if(activeEditor!==undefined){
+				text=activeEditor.document.getText(range);
+			}
+
+			fetchSelectedLines.push(`${text}${eol}`);
+		}else{
+			if(activeEditor!==undefined){
+				fetchSelectedLines.push(activeEditor.document.getText(selectedTextLine));
+			}
+		}
+	});
+
+
+	
+
+	if(fetchSelectedLines.length >0){
+		finalSelectedString = fetchSelectedLines[0];
+		finalSelectedString = finalSelectedString.trim();
+	}
+
+	return finalSelectedString;
+
+
+}
