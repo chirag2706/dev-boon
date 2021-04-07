1926614631b71113fa582f6ed5b9de930e32e0d4
Sripranav-m <m.sripranav@gmail.com>
2021-04-07 22:33:17 +0530

    Error query categorizing data

--- src/Python/PythonScript2.py
+++ src/Python/PythonScript2.py
@@ -1,55 +1,156 @@
 from googlesearch import search
 import requests
 from bs4 import BeautifulSoup
+from textblob import TextBlob
+
 
 query = "TypeError: search() got multiple values for argument 'tld'"
 
-req_links=[]
+req_links={}
 
-# for j in search(query, tld="co.in", num=12, stop=12, pause=2):
-#     if "stackoverflow" in j:
-#         req_links.append(j)
+for j in search(query, tld="co.in", num=12, stop=12, pause=2):
+    if "stackoverflow" in j:
+        req_links[j]=[]
 
+# dict has link and list as key and value
+# each value list contains
+# rating of question as 1st element ( based on custom parameters)
+# question as 2nd element
+# rating of accepted answer as 3rd element
+# accepted answer as 4th element
+# rating of best other answer as 5th element
+# best other answer as 6th element
 
-test_link="https://stackoverflow.com/questions/21764770/typeerror-got-multiple-values-for-argument"
-req_links.append(test_link)
 
 for link in req_links:
     r=requests.get(link)
     soup = BeautifulSoup(r.content, 'html5lib')
 
+    ######################################################################
+    #       QUESTION
+    ######################################################################
+
     question = soup.find('a', attrs = {'class':'question-hyperlink'}) 
     question=question.text
     #print(question)
 
-    DateCreated=soup.find('time' , attrs={'itemprop':"dateCreated"})
-    DateCreated=DateCreated.text
-    #print(DateCreated)
+    ######################################################################
 
-    Viewed=soup.find('div' , attrs={'class':"grid--cell ws-nowrap mb8"})
-    Viewed=Viewed.text
-    Viewed=Viewed.replace(" ","")
-    Viewed=Viewed.replace("\n","")
-    #print(Viewed)
+    #Number of Months passed after this question was asked
+    try:
+        DateCreated=soup.find('time' , attrs={'itemprop':"dateCreated"})
+        DateCreated=DateCreated.text
+        #print(DateCreated)
+        if "years" in DateCreated:
+            y=DateCreated.split("years")[0]
+            y=y.replace(" ","")
+            y=int(y)
+            y*=12
+        if "month" in DateCreated:
+            m=DateCreated.split("month")[0]
+            m=m.split(",")[1]
+            m=m.replace(" ","")
+            m=int(m)
+        time=m+y
+    except:
+        #assume question is asked 5 years ago
+        time=60
 
-    NumAnswers=soup.find("h2",attrs={"class":"mb0"})
-    NumAnswers=NumAnswers["data-answercount"]
-    #print(NumAnswers)
+    ######################################################################
 
-    #####################
+    #Number of Views to this question
+    try:
+        Viewed=soup.find('div' , attrs={'class':"grid--cell ws-nowrap mb8"})
+        Viewed=Viewed.text
+        Viewed=Viewed.replace(" ","")
+        Viewed=Viewed.replace("\n","")
+        #print(Viewed)
+        Viewed=Viewed.replace("Viewed","")
+        Viewed=Viewed.replace("times","")
+        Viewed=Viewed.replace(" ","")
+        Viewed=Viewed.replace(",","")
+        if "k" in Viewed:
+            Viewed=Viewed.replace("k","")
+            Viewed=Viewed*(1000)
+        if "m" in Viewed:
+            Viewed=Viewed.replace("m","")
+            Viewed=Viewed*(1000000)
+        Viewed=float(Viewed)
+        Viewed=int(Viewed)
+    except:
+        # assume 100 people viewed this
+        Viewed=100
+
+    ######################################################################
+
+    #Number of answers to this question
+    try:
+        NumAnswers=soup.find("h2",attrs={"class":"mb0"})
+        NumAnswers=NumAnswers["data-answercount"]
+        #print(NumAnswers)
+        NumAnswers=NumAnswers.replace(" ","")
+        NumAnswers=NumAnswers.replace(",","")
+        if "k" in NumAnswers:
+            NumAnswers=NumAnswers.replace("k","")
+            NumAnswers=NumAnswers*(1000)
+        if "m" in NumAnswers:
+            NumAnswers=NumAnswers.replace("m","")
+            NumAnswers=NumAnswers*(1000000)
+        NumAnswers=float(NumAnswers)
+        NumAnswers=int(NumAnswers)
+    except:
+        # assume there is 1 answer
+        NumAnswers=1
+
+    ######################################################################
 
     QuestionSoup=soup.find("div",{"class":"question"})
 
-    VoteCount=QuestionSoup.find("div",attrs={"itemprop":"upvoteCount"})
-    VoteCount=VoteCount.text
-    #print(VoteCount)
+
+    #Number of votes to this question
+    try:
+        VoteCount=QuestionSoup.find("div",attrs={"itemprop":"upvoteCount"})
+        VoteCount=VoteCount.text
+        #print(VoteCount)
+        VoteCount=VoteCount.replace(" ","")
+        VoteCount=VoteCount.replace(",","")
+        if "k" in VoteCount:
+            VoteCount=VoteCount.replace("k","")
+            VoteCount=VoteCount*(1000)
+        if "m" in VoteCount:
+            VoteCount=VoteCount.replace("m","")
+            VoteCount=VoteCount*(1000000)
+        VoteCount=float(VoteCount)
+        VoteCount=int(VoteCount)
+    except:
+        # assume there are 5 votes
+        VoteCount=5
+
+    ######################################################################
 
 
-    Author=QuestionSoup.find("div",attrs={"itemprop":"author"})
-    AuthorReputationScore=Author.find("span",attrs={'class':"reputation-score"})
-    AuthorReputationScore=AuthorReputationScore.text
-    #print(AuthorReputationScore)
+    #Question author reputation score
+    try:
+        Author=QuestionSoup.find("div",attrs={"itemprop":"author"})
+        AuthorReputationScore=Author.find("span",attrs={'class':"reputation-score"})
+        AuthorReputationScore=AuthorReputationScore.text
+        AuthorReputationScore=AuthorReputationScore.replace(" ","")
+        AuthorReputationScore=AuthorReputationScore.replace(",","")
+        if "k" in AuthorReputationScore:
+            AuthorReputationScore=AuthorReputationScore.replace("k","")
+            AuthorReputationScore=AuthorReputationScore*(1000)
+        if "m" in AuthorReputationScore:
+            AuthorReputationScore=AuthorReputationScore.replace("m","")
+            AuthorReputationScore=AuthorReputationScore*(1000000)
+        AuthorReputationScore=float(AuthorReputationScore)
+        AuthorReputationScore=int(AuthorReputationScore)
+    except:
+        # assume the reputation score as 5
+        AuthorReputationScore=5
 
+    ######################################################################
+    #       ACCEPTED ANSWER
+    ######################################################################
 
     comments=[]
     Comment=QuestionSoup.find("div",{"class":"js-post-comments-component"})
@@ -67,35 +168,36 @@ for link in req_links:
     a={}
     Answers=AnswerSoup.find_all("div",{"itemprop":"acceptedAnswer"})
 
-    for answer in Answers:
-        a={}
-        VoteCount=answer.find("div",attrs={"itemprop":"upvoteCount"})
-        VoteCount=VoteCount.text
-        a["Upvotes"]=VoteCount
+    a={}
+    VoteCount=Answers.find("div",attrs={"itemprop":"upvoteCount"})
+    VoteCount=VoteCount.text
+    a["Upvotes"]=VoteCount
 
-        ans=answer.find("div",attrs={"class":"s-prose js-post-body"})
-        a["Answer"]=ans.text
-        print(a["Answer"])
+    ans=Answers.find("div",attrs={"class":"s-prose js-post-body"})
+    a["Answers"]=ans.text
+    #print(a["Answers"])
 
-        ans=answer.find("div",attrs={"itemprop":"author"})
-        AuthorReputationScore=answer.find("span",attrs={'class':"reputation-score"})
-        AuthorReputationScore=AuthorReputationScore.text
-        a["ReputationScore"]=AuthorReputationScore
+    ans=Answers.find("div",attrs={"itemprop":"author"})
+    AuthorReputationScore=Answers.find("span",attrs={'class':"reputation-score"})
+    AuthorReputationScore=AuthorReputationScore.text
+    a["ReputationScore"]=AuthorReputationScore
 
-        comments=[]
-        Comment=answer.find("div",{"class":"js-post-comments-component"})
-        Comment=Comment.find_all("li",{"class":"comment js-comment"})
-        for c in Comment:
-            cc=c.find("span",{"class":"comment-copy"})
-            cc=cc.text
-            comments.append(cc)
-        a["Comments"]=comments
+    comments=[]
+    Comment=Answers.find("div",{"class":"js-post-comments-component"})
+    Comment=Comment.find_all("li",{"class":"comment js-comment"})
+    for c in Comment:
+        cc=c.find("span",{"class":"comment-copy"})
+        cc=cc.text
+        comments.append(cc)
+    a["Comments"]=comments
 
-        accanswer.append(a)
+    accanswer.append(a)
 
     #print(accanswer)
 
-    
+    ######################################################################
+    #       OTHER ANSWER
+    ######################################################################
 
 
     answers=[]
@@ -126,5 +228,6 @@ for link in req_links:
         a["Comments"]=comments
 
         answers.append(a)
+
 
     #print(answers)
