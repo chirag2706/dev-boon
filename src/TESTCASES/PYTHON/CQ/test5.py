5a78253fec2456e709ad71afb2ae90fdaa4690e4
Sripranav-m <m.sripranav@gmail.com>
2021-04-22 16:29:14 +0530

    Test Cases

---
+++ src/TESTCASES/PYTHON/CQ/test5.py
@@ -0,0 +1,15 @@
+ def NthfibonacciNumber(num):
+    """
+    >>> nthfibonacci(100)
+    354224848179261915075
+    >>> nthfibonacci(100_000)
+    354224848179261915075
+    """
+    gen = fibonacci_generator()
+    while len(str(next(gen))) < n:
+        next = int(next % 10)
+        gen += 1
+    return gen
+
+
+
